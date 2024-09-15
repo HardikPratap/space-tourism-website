539cab88068486f14e04ec0887ce488d3f1cd150
Hardik Pratap <hardikpratap188@gmail.com>
2024-09-15 07:45:06 +0530

    images constant in script

--- script.js
+++ script.js
@@ -19,3 +19,53 @@ const techSlider = document.querySelector(".slider");
 const allTechSlide = document.querySelectorAll(".tech");
 const techImg = document.querySelector(".tech-img img");
 // DOM
+
+// Images
+const images = {
+    bgImagesDesktop: [
+      "starter-code/assets/home/background-home-desktop.jpg",
+      "starter-code/assets/destination/background-destination-desktop.jpg",
+      "starter-code/assets/crew/background-crew-desktop.jpg",
+      "starter-code/assets/technology/background-technology-desktop.jpg",
+    ],
+  
+    bgImagesTablet: [
+      "starter-code/assets/home/background-home-tablet.jpg",
+      "starter-code/assets/destination/background-destination-tablet.jpg",
+      "starter-code/assets/crew/background-crew-tablet.jpg",
+      "starter-code/assets/technology/background-technology-tablet.jpg",
+    ],
+  
+    bgImagesMobile: [
+      "starter-code/assets/home/background-home-mobile.jpg",
+      "starter-code/assets/destination/background-destination-mobile.jpg",
+      "starter-code/assets/crew/background-crew-mobile.jpg",
+      "starter-code/assets/technology/background-technology-mobile.jpg",
+    ],
+  
+    destinationImages: [
+      "starter-code/assets/destination/image-moon.png",
+      "starter-code/assets/destination/image-mars.png",
+      "starter-code/assets/destination/image-europa.png",
+      "starter-code/assets/destination/image-titan.png",
+    ],
+  
+    crewImages: [
+      "starter-code/assets/crew/image-douglas-hurley.png",
+      "starter-code/assets/crew/image-mark-shuttleworth.png",
+      "starter-code/assets/crew/image-victor-glover.png",
+      "starter-code/assets/crew/image-anousheh-ansari.png",
+    ],
+  
+    technologyPortraitImages: [
+      "starter-code/assets/technology/image-launch-vehicle-portrait.jpg",
+      "starter-code/assets/technology/image-spaceport-portrait.jpg",
+      "starter-code/assets/technology/image-space-capsule-portrait.jpg",
+    ],
+  
+    technologyLandscapeImages: [
+      "starter-code/assets/technology/image-launch-vehicle-landscape.jpg",
+      "starter-code/assets/technology/image-spaceport-landscape.jpg",
+      "starter-code/assets/technology/image-space-capsule-landscape.jpg",
+    ],
+  };
