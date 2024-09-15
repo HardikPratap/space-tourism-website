1b04d8fd0b3873fdb34dfb21ac26b84da59174e8
Hardik Pratap <hardikpratap188@gmail.com>
2024-09-15 07:30:25 +0530

    added dom in script file

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// DOM
+const navbar = document.querySelector(".navbar");
+const navLinks = document.querySelector(".nav__links");
+const links = document.querySelectorAll(".nav__link");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const sections = document.querySelectorAll("section");
+const destinationNav = document.querySelectorAll(".destination__nav");
+const destinationNavLinks = document.querySelectorAll(".nav .nav__link");
+const explore = document.querySelector(".explore");
+const columnImg = document.querySelector(".column img");
+const sliderImg = document.querySelector(".slider--img img");
+const slides = document.querySelectorAll(".slide");
+const dotContainer = document.querySelector(".dots");
+const buttons = document.querySelector(".buttons");
+const btns = document.querySelectorAll(".btn");
+const techSlider = document.querySelector(".slider");
+const allTechSlide = document.querySelectorAll(".tech");
+const techImg = document.querySelector(".tech-img img");
+// DOM
